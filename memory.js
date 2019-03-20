71805e35342032a5264c26efe338248b2fe30a0b
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-03-20 15:59:13 -0700

    basic flip outline

--- memory.js
+++ memory.js
@@ -1,5 +1,7 @@
 window.onload=function(){
-    var array = ['1','2','3','4','5','6','7','8','9','10','11','12']
+    var array = ['1','2','3','4','5','6','7','8','9','10','11','12'];
+    var match = [];
+    var matched = 0;
     function shuffle(a) {
         var j, x, i;
         for (i = a.length - 1; i > 0; i--) {
@@ -10,6 +12,33 @@ window.onload=function(){
         }
         return a;
     }
+    function noMatch { // make class front visible
 
+    }
+    function flip (e){
+        e.preventdefault();
+        
+        if (match.length === 1){    // 2nd click
+            if (e.target.classname === match[0]){  //match
+                matched++                   // number of matches
+                if (matched >= 6) {
+                    winner          // result screen if I have time
+                }
+            }
+            else {
+                (setTimeout(function(){noMatch()}, 1000,ms)  //flip back
+            }
+        }
+        else{ // 1st or 3rd click
+            if (match === undefined){ // 1st click
+                match.push(e.target.classname)
+            }
+        }
+            else{                   // 3rd click (aka they clicked again before 1 sec timeout fired)
+                noMatch();
+                flip(e)
+            }
+        }
+    }
 
 }
