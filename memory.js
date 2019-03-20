7be991b36564db4e97edf7b20a241c3465250ba5
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-03-20 14:46:05 -0700

    win onload has to be equal to a function

---
+++ memory.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    var array = ['1','2','3','4','5','6','7','8','9','10','11','12']
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+
+}
